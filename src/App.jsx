96ac71e04dc393903d31128a7196f51bd64bc160
phubetthongbai <nicekup07@gmail.com>
2024-03-01 10:55:04 +0700

    .newUpdate

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hello from "./assets/Hello";
+import Contact from "./assets/Contact";
+import Counter from "./assets/Counter";
+
+function App() {
+  console.log('App component');
+  return (
+    <div className='App'>
+      < Counter />
+      <Hello name="Phubet" message="Hi there"/>
+      <Hello name="Nice" message="Hello.."/>
+      <Contact email="nice@gmail.com" phone="0988848052" />
+    </div>
+  );
+}
+
+export default App;
